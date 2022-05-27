a533e428cf258e4871e763e880249bb803e062ef
Sharthak <sharthakrai@outlook.com>
2022-05-27 13:30:51 +0100

    p

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
-    
+var audio = new Audio('sounds/tom-1.mp3');
 
 document.querySelector('button').addEventListener('click', function () {
-    alert('click');
+
 
     //click all button
     var buttons = document.querySelectorAll('.drum');
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function () {
-            alert('click');
+            
+
+            audio.play();
+            
+
         });
     }
 
